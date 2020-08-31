1153e3905288327948a7d062f8dc8aef3c3171fa
Daylan <daylanberry@gmail.com>
2020-08-31 16:31:31 -0700

    created router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])}/>
-    </div>
+    <Switch>
+      <Route exact path='/' render={() => <h1>palette list goes here</h1>}/>
+      <Route exact path='/palette/:id' />
+    </Switch>
+
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedColors[4])}/>
+    // </div>
   );
 }
 
--- src/Palette.js
+++ src/Palette.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import ColorBox from './ColorBox'
-import './Palette.css'
-import Navbar from './Navbar'
+import ColorBox from './ColorBox';
+import './Palette.css';
+import Navbar from './Navbar';
+import Snackbar from '@material-ui/core/Snackbar';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+
 
 class Palette extends Component {
 
@@ -10,7 +14,8 @@ class Palette extends Component {
 
     this.state = {
       level: 500,
-      format: 'hex'
+      format: 'hex',
+      open: false
     }
   }
 
@@ -18,16 +23,24 @@ class Palette extends Component {
     this.setState({ level })
   }
 
-  handleChange = (val) => {
-    this.setState({ format: val })
+  handleFormatChange = (val) => {
+    this.setState({ format: val, open: true })
+  }
+
+  closeSnackBar = () => {
+    this.setState({ open: false})
   }
 
   render() {
-    const { colors } = this.props.palette;
-    const { level, format } = this.state;
+    const { colors, paletteName, emoji } = this.props.palette;
+    const { level, format, open } = this.state;
 
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color[format]} name={color.name}/>
+      <ColorBox
+        background={color[format]}
+        name={color.name}
+        key={color.name}
+      />
 
     ))
     return (
@@ -35,11 +48,34 @@ class Palette extends Component {
         <Navbar
           changeLevel={this.changeLevel}
           level={level}
-          handleChange={this.handleChange}
+          handleChange={this.handleFormatChange}
         />
         <div className='Palette-colors'>
           {colorBoxes}
         </div>
+        <Snackbar
+          anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
+          open={open}
+          message={<span id='message-id'>Format changed to {format.toUpperCase()}</span>}
+          ContentProps={{
+            'aria-describedby': 'message-id'
+          }}
+          onClose={this.closeSnackBar}
+          action={[
+            <IconButton
+              onClick={this.closeSnackBar}
+              color='inherit'
+              key='close'
+              aria-label='close'
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
+        <footer className='Palette-footer'>
+          {paletteName}
+          <span className='emoji'>{emoji}</span>
+        </footer>
       </div>
     )
   }
